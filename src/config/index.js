07037355ab028d2f47ae8dd15969d4565a748498
Thien Tran <thien.tran@dfo.global>
2021-06-13 00:59:49 +0700

    Replace mongoUrl by real url

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb+srv://trquth:25011993Tran@cluster0.hr1pi.mongodb.net/chatdemo?retryWrites=true&w=majority",
   "bodyLimit": "100kb"
 }
